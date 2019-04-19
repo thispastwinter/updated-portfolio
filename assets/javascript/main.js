1c56d6f6f0d2c5b2655ef6a663a47e074110a7f6
Justin Klaas <klaas.justin@gmail.com>
2019-04-19 08:49:03 -0500

    working?

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,4 +1,8 @@
  $(document).ready(function () {
-   $('.sidenav').sidenav();
-   $('.parallax').parallax();
+   $('.carousel').carousel();
+ });
+
+ $('.carousel.carousel-slider').carousel({
+   fullWidth: true,
+   indicators: true
  });
