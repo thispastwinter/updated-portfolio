f3a68a690e77eec9c08be48eb20f79af5a3f3c50
Justin Klaas <46500734+thispastwinter@users.noreply.github.com>
2019-06-15 02:34:44 -0500

    form working with modals and some validation

--- public/assets/javascript/contact.js
+++ public/assets/javascript/contact.js
@@ -3,36 +3,47 @@ const paragraph = document.getElementById('info');
 const passOrFail = document.getElementById('pass-or-fail');
 
 $('.submit').on('click', function () {
-  const name = $('#name').val();
-  const email = $('#email').val();
-  const message = $('#message').val();
-  console.log(name, email, message);
-  $.post('/', {
-    type: 'POST',
-    name: name,
-    email: email,
-    message: message
-  }).then(function (data) {
-    if (data === 'success') {
-      modal.style.display = 'block';
-      passOrFail.innerHTML = 'Success!'
-      paragraph.innerHTML = 'Your Message Has Been Sent!';
-      window.onclick = function (event) {
-        if (event.target == modal) {
-          modal.style.display = 'none';
+  if ($('#name').val() === '' || $('#email').val() === '' || $('#message').val() === '') {
+    modal.style.display = 'block';
+    passOrFail.innerHTML = 'Attention!'
+    paragraph.innerHTML = 'You must fill out every field.'
+  } else {
+    $('.submit').html('<i class="fas fa-spin fa-circle-notch"></i>');
+    const name = $('#name').val();
+    const email = $('#email').val();
+    const message = $('#message').val();
+    console.log(name, email, message);
+    $.post('/', {
+      type: 'POST',
+      name: name,
+      email: email,
+      message: message
+    }).then(function (data) {
+      $('.submit').text('Submit')
+      if (data === 'success') {
+        modal.style.display = 'block';
+        passOrFail.innerHTML = 'Success!'
+        paragraph.innerHTML = 'Your Message Has Been Sent.';
+        $('#name').val('');
+        $('#email').val('');
+        $('#message').val('');
+        window.onclick = function (event) {
+          if (event.target == modal) {
+            modal.style.display = 'none';
+          }
+        }
+      } else {
+        modal.style.display = 'block';
+        passOrFail.innerHTML = 'Attention!'
+        paragraph.innerHTML = 'We apologize, but your message can not be sent at this time!';
+        window.onclick = function (event) {
+          if (event.target == modal) {
+            modal.style.display = 'none';
+          }
         }
       }
-    } else {
-      modal.style.display = 'block';
-      passOrFail.innerHTML = 'Attention!'
-      paragraph.innerHTML = 'We apologize, but your message can not be sent at this time!';
-      window.onclick = function (event) {
-        if (event.target == modal) {
-          modal.style.display = 'none';
-        }
-      }
-    }
-  });
+    });
+  }
 })
 
 $('#modal-close').on('click', function () {
